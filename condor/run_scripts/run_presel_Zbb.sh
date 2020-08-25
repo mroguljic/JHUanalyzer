35e9841aab47c6268b4dc033990631b1e5da2312
Matej Roguljic matej.roguljic@cern.ch <matej.roguljic@gmail.com>
2020-08-25 14:38:19 +0200

    switched architecture to slc7

--- condor/run_scripts/run_presel_Zbb.sh
+++ condor/run_scripts/run_presel_Zbb.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 xrdcp root://cmseos.fnal.gov//store/user/lcorcodi/10XwithNano.tgz ./
-export SCRAM_ARCH=slc6_amd64_gcc700
-scramv1 project CMSSW CMSSW_10_2_13
+export SCRAM_ARCH=slc7_amd64_gcc700
+scramv1 project CMSSW CMSSW_10_6_14
 tar xzf 10XwithNano.tgz
 rm 10XwithNano.tgz
-cd CMSSW_10_2_13/src
+cd CMSSW_10_6_14/src
 eval `scramv1 runtime -sh`
+echo "git clone https://github.com/mroguljic/JHUanalyzer.git -b matejHbb"
 git clone https://github.com/mroguljic/JHUanalyzer.git -b matejHbb
 cd ../..
 
@@ -14,13 +15,11 @@ mkdir tardir; cp tarball.tgz tardir/; cd tardir
 tar xzvf tarball.tgz
 echo "I AM HERE"
 pwd
-cp -r * ../CMSSW_10_2_13/src/JHUanalyzer/
-cd ../CMSSW_10_2_13/src/JHUanalyzer/
+cp -r * ../CMSSW_10_6_14/src/JHUanalyzer/
+cd ../CMSSW_10_6_14/src/JHUanalyzer/
 eval `scramv1 runtime -sh`
 
 echo make_preselection_HbbX.py $*
 echo $*
 python make_preselection_HbbX.py $*
 ls -lrth
-cp Hbbpreselection*.root ../../../
-
